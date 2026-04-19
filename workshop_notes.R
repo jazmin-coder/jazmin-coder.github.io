701ef7199fa3fe42004fb616eb2e72eef565444f
Jazmin <jazmincalimano@gmail.com>
2026-04-19 17:03:25 -0500

    updating site

--- workshop_notes.R
+++ workshop_notes.R
@@ -1,3 +1,11 @@
 #Workshop Notes
 
 #I'm adding a comment to practice committing changes using Git.
+
+#Local and remote repositories are in sync. 
+
+#To update files in remote repository, follow these commands at the terminal each time:
+#Save the file locally.
+#Run: git add .
+#Run: git commit -m "message"
+#Run: git push
